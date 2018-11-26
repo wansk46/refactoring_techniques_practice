2cff708c5c2fa635b9ebaa242a7ba4ae782555f0
Wan Yuen Yi <wanyuenyi@Wans-MacBook-Air.local>
2018-11-27 02:58:53 +0800

    Test case of BannerRander

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -1,10 +1,16 @@
 package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
+    private boolean isRendered = false;
     void renderBanner(String platform, String browser) {
         if ((platform.toUpperCase().indexOf("MAC") > -1) &&
                 (browser.toUpperCase().indexOf("IE") > -1)) {
             // do something
+            isRendered = true;
         }
+    }
+
+    boolean IsRendered(){
+        return this.isRendered;
     }
 }
--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -9,10 +9,30 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class BannerRenderTest {
 
+    @Test
+    public void should_not_rendered(){
+        BannerRender bannerRender = new BannerRender();
+        String platform = "this is platform";
+        String browser = "this is browser";
 
+        bannerRender.renderBanner(platform, browser);
+        assertFalse(bannerRender.IsRendered());
+    }
+
+    @Test
+    public void should_rendered(){
+        BannerRender bannerRender = new BannerRender();
+        String platform = "this is mac";
+        String browser = "this is ie";
+
+        bannerRender.renderBanner(platform, browser);
+        assertTrue(bannerRender.IsRendered());
+    }
 
 }
