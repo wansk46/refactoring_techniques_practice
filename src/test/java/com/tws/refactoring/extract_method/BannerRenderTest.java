4d5d7580d99dfb4efb9017546cc055de512496b5
Wan Yuen Yi <wanyuenyi@Wans-MacBook-Air.local>
2018-11-27 02:28:12 +0800

    [wansk]Owing Printer

---
+++ src/test/java/com/tws/refactoring/extract_method/BannerRenderTest.java
@@ -0,0 +1,18 @@
+package com.tws.refactoring.extract_method;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class BannerRenderTest {
+
+
+
+}
