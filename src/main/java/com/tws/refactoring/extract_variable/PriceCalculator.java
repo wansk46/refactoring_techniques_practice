584aab615bbfac9ab7f91eb9ef10ee72fbf8329e
Wan Yuen Yi <wanyuenyi@Wans-MacBook-Air.local>
2018-11-27 02:47:46 +0800

    PriceCalculatorRefractorDiscount

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -4,8 +4,9 @@ public class PriceCalculator {
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
         int basePrice = quantity * itemPrice;
+        double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
         return basePrice-
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
+                discount +
                 Math.min(quantity * itemPrice * 0.1, 100.0);
     }
 }
